e2bf09d51b8016374cd6c528397a5d5d9a1182e3
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2012-12-14 16:03:52 +0200

    Refactoring and added time caching
    Accurate time isn't needed for GeoField calculation

--- src/main/scala/hlz/klusterisavior/KlusteriLocation.scala
+++ src/main/scala/hlz/klusterisavior/KlusteriLocation.scala
@@ -2,28 +2,29 @@ package hlz.klusterisavior
 
 import android.location.Location
 import android.hardware.GeomagneticField
-import compat.Platform
 
 object KlusteriLocation {
-  lazy val klusteriLocation = {
+  val klusteriLocation = {
     val l = new Location("static")
-    l.setLatitude(60.16944)
-    l.setLongitude(24.92162)
+    l.setLatitude(60.169439353910285)
+    l.setLongitude(24.921391010284424)
     l
   }
 
-  def bearingToKlusteri(azimuth: Double, currentLocation: Location) = {
-    def magneticNorthToTrueNorth(azimuth: Double) = {
-      val geoField = new GeomagneticField(
-        currentLocation.getLatitude.toFloat,
-        currentLocation.getLongitude.toFloat,
-        currentLocation.getAltitude.toFloat,
-        Platform.currentTime
-      )
-      azimuth + geoField.getDeclination
-    }
-    val trueAzimuth = magneticNorthToTrueNorth(azimuth)
-    trueAzimuth - currentLocation.bearingTo(klusteriLocation)
+  private val approximateCurrentTime = System.currentTimeMillis()
+
+  def magneticNorthToTrueNorth(location: Location, azimuth: Double) = {
+    val geoField = new GeomagneticField(
+      location.getLatitude.toFloat,
+      location.getLongitude.toFloat,
+      location.getAltitude.toFloat,
+      approximateCurrentTime
+    )
+    azimuth + geoField.getDeclination
   }
 
+  def bearingToKlusteri(azimuth: Double, currentLocation: Location) = {
+    val trueAzimuth = magneticNorthToTrueNorth(currentLocation, azimuth)
+    trueAzimuth - currentLocation.bearingTo(klusteriLocation)
+  }
 }

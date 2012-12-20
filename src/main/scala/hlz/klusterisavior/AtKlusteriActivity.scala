cbf9d860436d3bdfd54a5577baa75b507e2c581e
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2012-12-20 17:37:55 +0200

    Simple Chronometer WIP

--- src/main/scala/hlz/klusterisavior/AtKlusteriActivity.scala
+++ src/main/scala/hlz/klusterisavior/AtKlusteriActivity.scala
@@ -4,9 +4,12 @@ import android.app.Activity
 import android.os.Bundle
 import android.content.Context
 import android.text.format.Time
+import android.text.format
+import android.util.TimeFormatException
 
 class AtKlusteriActivity extends Activity with TypedActivity {
   private lazy val preferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+  private lazy val chronometer = findView(TR.chronometer)
 
   def setLastTimeAtKlusteri(time: Time) {
     val prefEditor = preferences.edit()
@@ -24,5 +27,19 @@ class AtKlusteriActivity extends Activity with TypedActivity {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.at_klusteri)
     setLastTimeAtKlusteriToNow()
+  }
+
+  override def onStart() {
+    super.onStart()
+    val lastTimeString = preferences.getString("LAST_TIME_AT_KLUSTERI", "")
+    val lastTime = new format.Time()
+    try {
+      lastTime.parse3339(lastTimeString)
+      chronometer.setBase(lastTime.toMillis(false))
+    } catch {
+      case ex: TimeFormatException =>
+    }
+
+    chronometer.start()
   }
 }
